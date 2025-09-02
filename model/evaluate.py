4b08dce4e82a3fadc2b02e277da35c2be8086d2a
nextgautam <gautamyadav1406@gmail.com>
2025-09-02 15:23:00 +0530

    Initial Commit 1.0

--- model/evaluate.py
+++ model/evaluate.py
@@ -1,15 +1,11 @@
+#evaluate.py
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import joblib
 
-import os
-
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))  # go one level up
-data_path = os.path.join(BASE_DIR, 'data', 'iris.csv')
-
 # Load the dataset
-data = pd.read_csv(data_path)
+data = pd.read_csv('data/iris.csv')
 
 # Preprocess the dataset
 X = data.drop('species', axis=1)
@@ -17,11 +13,8 @@ y = data['species']
 
 # Split the data into training and test sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
 # Load the saved model
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))  # go one level up
-data_path = os.path.join(BASE_DIR, 'iris_model.pkl')
-model = joblib.load(data_path)
+model = joblib.load('model/iris_model.pkl')
 
 # Make predictions
 y_pred = model.predict(X_test)
